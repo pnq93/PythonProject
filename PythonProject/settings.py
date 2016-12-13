3fc6312554e1ca410b124a5245f990d53e056f17
Bartek <bartosz.bobrek1993@gmail.com>
2016-12-13 03:41:46 +0100

    Added Bootstrap and AdminLTE and view including first chart with random values

--- PythonProject/settings.py
+++ PythonProject/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os
+import os.path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,6 +115,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+STATICFILES_DIRS = (
+    os.path.join(
+        BASE_DIR,
+        'static',
+    ),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
